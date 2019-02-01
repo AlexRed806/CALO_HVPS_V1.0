be66d17b7b1a040f27d123f895cb91063aae2b6d
Alessandro Minotti <aethelfirt86@gmail.com>
2019-02-01 19:32:05 +0100

    test, pointers..

--- src/CALO_HVPS.cpp
+++ src/CALO_HVPS.cpp
@@ -508,7 +508,7 @@ int CALO_HVPS::cmd(const std::string& command, int commandStringAck, std::string
                || ( *it._location_channel_.row == rowNumber && chaine.find("SetNominalRow")==0 )
                || chaine.find("SetNominalAll")==0 ) {
                 
-                setChannel(RacineElementCh,*it.nominal_voltage);
+                setChannel(RacineElementCh,it->nominal_voltage);
                 startChannel(RacineElementCh);
             }
         }
